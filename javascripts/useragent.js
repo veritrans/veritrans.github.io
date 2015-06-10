df71c24acaf1a2d4c093f50561a3f164a86e0fc5
alditayurio <aldita.yurioputri@veritrans.co.id>
2015-06-10 19:10:23 +0700

    change font to source sans pro

--- javascripts/useragent.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){"Win16"==navigator.platform||"Win32"==navigator.platform||"WinCE"==navigator.platform?$("body").css("font-weight","400"):$("body").css("font-weight","300")});
